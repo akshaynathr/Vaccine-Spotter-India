9634cfe434d96c0d5b680102654a59fddfecb468
multitude0099 <suryapratap0099@gmail.com>
2021-05-07 19:44:40 +0530

    unknown sender bug fix and readme cleaned

--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
-# Vaccine spotter for India using pincode
+# Vaccine spotter for India
 
 Vaccine spotter is a simple tool for tracking the availability of Covid vaccines in any state in India by pincode or district code.
-It uses the [api](https://apisetu.gov.in/public/marketplace/api/cowin/cowin-public-v2#/Appointment%20Availability%20APIs/findByPin) from COWIN site to monitor for vaccine availability and sent an immediate email :envelope: to user.
+It uses the [api](https://apisetu.gov.in/public/marketplace/api/cowin/cowin-public-v2#/Appointment%20Availability%20APIs/) from COWIN site to monitor for vaccine availability and sent an immediate email :envelope: to user.
  ✨
 
 ## Features
@@ -9,7 +9,8 @@ It uses the [api](https://apisetu.gov.in/public/marketplace/api/cowin/cowin-publ
 - Check availability of vaccine by providing pincode or district code of your area
 - Run in the terminal and monitor the vaccine availability
 - Send email to email address set by user immediately when there is vaccines available
-- set age range to check availability in that range.
+- Set age range to check availability in that range.
+- Set wait time before querying again the api
 
 
 ## Set Up
@@ -29,42 +30,25 @@ for other mail providers the smpt address should be modified.
 
 ## Usage
 For running these scripts you've to set the email details in [config file](config.yml) file.
-Edit following part of code in [config file](config.yml) file
-``` sh 
-email:
-  sent_from : "<sender_email>" 
-  email_password : "<sender_password>"
+Edit following part of [config file](config.yml#L2-6) file. You can also edit the wait time [here](config.yml#L18)
 
-  # [edit] enter receiver email details
-  to : ["<receiver_email_id>"]
-```
 
 There are two ways in which you can see vaccine availability
 
-### Using pincode.
-Set your area pincode in [config file](config.yml) file as shown below. Find pincode of your area [here](https://www.indiapost.gov.in/VAS/Pages/findpincode.aspx)
-``` sh 
-area_info:
-# [edit] enter your district code or pincode
-  __district_code : "<district_code>" 
-  __pincode : "<pincode>"
-```
+### Using pincode
+Set your area pincode [here](config.yml#L11). Find pincode of your area [here](https://www.indiapost.gov.in/VAS/Pages/findpincode.aspx)
 
-Edit [vaccineSpotter](vaccineSpotter.py) file and set 
+Set query type to `"pincode"`: [here](config.yml#L17) 
 ```sh
-query_type = "pincode"
+query_type : "pincode"
 ```
 
-Run  
-```sh
-python3 vaccineSpotter.py
-
-```
+Run `python3 vaccineSpotter.py`. 
 Then it'll search for vaccine availability in your area.
 
 
 ### Using district code
-Set district code in the [config file](config.yml) file.
+Set query type to `"district_code"`: [here](config.yml#L17). Set your district code [here](config.yml#L10)..
 To know your district code follow these steps:
 
 - First see your state code [here](https://cdn-api.co-vin.in/api/v2/admin/location/states) 
@@ -72,27 +56,10 @@ To know your district code follow these steps:
 - Now edit your state code in this url https://cdn-api.co-vin.in/api/v2/admin/location/districts/{your_state_code} 
   e.g. https://cdn-api.co-vin.in/api/v2/admin/location/districts/16 for karnataka
 
-- click on the url for your state you can see your district code by searching your district name.
+- Click on the url for your state you can see your district code by searching your district name.
 
-
-Enter the district code in [config file](config.yml) file
-
-``` sh 
-
-area_info:
-# [edit] enter your district code or pincode
-  __district_code : "<district_code>"
-``` 
-Edit [vaccineSpotter](vaccineSpotter.py) file and set 
-```sh
-query_type = "district_code"
-```
-Run the script after setting the values 
-```sh
-python3 vaccineSpotter.py
-
-```
-It'll search for availibility of vaccine centers in that area.
+Run `python3 vaccineSpotter.py`. 
+Then it'll search for vaccine availability in your area.
 
 
 ## Development
--- vaccineSpotter.py
+++ vaccineSpotter.py
@@ -7,12 +7,16 @@ import yaml
 
 
 class vaccineSpotter:
-	def __init__(self, config_file_path, time_delay=1):
+	def __init__(self, config_file_path):
 		self.config_file_path = config_file_path
-		self.time_delay = time_delay
 		self.cfg = self.read_config()
 		self.set_params()
 
+		## root url and headers
+		self.root_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public"
+		self.headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
+
+	# read config file
 	def read_config(self):
 		with open(self.config_file_path, "r") as ymlfile:
 			cfg = yaml.safe_load(ymlfile)
@@ -22,6 +26,8 @@ class vaccineSpotter:
 		## params
 		self.email_info = self.cfg["email"]
 		self.area_info = self.cfg["area_info"]
+		self.age_limit_info = self.cfg["age_limit"]
+		self.query_info = self.cfg["query_info"]
 		
 		## sender mail info
 		self.sent_from = self.email_info['sent_from']
@@ -36,24 +42,26 @@ class vaccineSpotter:
 		self.__pincode = self.area_info['__pincode']
 
 		#age limit for vaccination
-		self.age_limit_info = self.cfg['age_limit']
 		self.age_limit = self.age_limit_info['age_limit']
 
-	def send_email(self, result):
+		# query type and time delay for querying the cowin site
+		self.query_type = self.query_info['query_type']
+		self.wait_time = self.query_info['wait_time']
+
+
+	def send_email(self, res):
 	# turn on allow less secure apps to get email
 	#  https://myaccount.google.com/lesssecureapps
 	# suggest to use a backup account for this to preserve security
 	
 		subject = 'Vaccine slot available in your area'
-		body = "Following vaccines centers are found \n\n Query Time : \
-				 "+ctime(time())+"\n\n" + result
-
+		body = "Following vaccines centers are found \n\n Query Time :  "+ctime(time())+"\n\n" + res
 		email_text = """\
-	From: %s
-	To: %s 
-	Subject: %s
-	%s
-	""" % (self.sent_from, ", ".join(self.to), subject, body)
+From: %s
+To: %s 
+Subject: %s
+%s
+""" % (self.sent_from, ", ".join(self.to), subject, body)
 		print(email_text)
 
 		try:
@@ -63,12 +71,16 @@ class vaccineSpotter:
 			server.sendmail(self.sent_from, self.to, email_text)
 			server.close()
 
-			print('Email sent!\n')
+			print('Email sent!')
 		except Exception as e:
-			print('Something went wrong...')
+			print('Something went wrong while sending the email...')
 			print (e)
 
+
 	def parse_json_district_code(self, result):
+		"""
+			parse json pbtained from the CoWIN API using district code
+		"""
 		output = []
 		centers = result['centers']
 		for center in centers:
@@ -84,6 +96,9 @@ class vaccineSpotter:
 
 
 	def parse_json_pincode(self, result):
+		"""
+			parse json pbtained from the CoWIN API using pincode
+		"""
 		output = []
 		sessions = result['sessions']
 		if len(sessions)==0:
@@ -98,6 +113,10 @@ class vaccineSpotter:
 		return output
 
 	def call_api(self, url, headers, query_type):
+		"""
+			call CoWIN API and exteact vaccination centre details
+		"""
+
 		response = requests.get(url, headers = headers)
 		if response.status_code == 200:
 			print("API call success")
@@ -122,16 +141,21 @@ class vaccineSpotter:
 					result_str = result_str + "age_limit:"+str(center['age_limit'])+"\n"
 					result_str = result_str + "-----------------------------------------------------\n"
 				self.send_email(result_str)
+				print("\nTrying again after {} minutes.....\n".format(self.wait_time))
 
 			else:
-				print("Vaccines not available for age limit {}\nTrying again\
-				 after {} minute.....\n".format(*self.age_limit, self.time_delay))
+				print("Vaccines not available for age limit {}\nTrying again...\
+					after {} minutes.....\n".format(*self.age_limit, self.wait_time))
 		else:
-			print("something went wrong :(\nStatus code {} \nTrying again......\
-				after {} minute.....\n".format(response.status_code, self.time_delay))
+			print("something went wrong :(\nStatus code {} \nTrying again...\
+				after {} minutes.....\n".format(response.status_code, self.wait_time))
 
 
 	def query(self, root_url, headers, query_type):
+		"""
+			requests the CoWIN site for vaccination availability depending on the query_type
+		"""
+
 		print(ctime(time()))
 		
 		# format date
@@ -139,7 +163,7 @@ class vaccineSpotter:
 		d1 = today.strftime("%d/%m/%Y")
 		__date = str(d1).replace("/","-")
 
-
+		# check for query type
 		if query_type == 'district_code':
 			url = root_url + "/calendarByDistrict?district_id=" + self.__district_code + "&date="+ __date
 
@@ -149,24 +173,26 @@ class vaccineSpotter:
 			print('incorrect query type\nquery type must be either district_code or pincode\n')
 			return
 		self.call_api(url,  headers, query_type)
+		return
 
+	def main(self):
+		"""main function for querying the CoWIN site"""
+		curr_t = datetime.now()
+		print("querying by {} .....".format(self.query_type))
+				
+		# query the CoWIN site
+		self.query(self.root_url, self.headers, self.query_type)
 
-t = datetime.now()
+		## query the cowin site after every time_delay minutes
+		while True:
+			delta = datetime.now() - curr_t
+			if delta.seconds >= self.wait_time * 60:
+				self.query(self.root_url, self.headers, self.query_type)
+				curr_t = datetime.now()
+
 if __name__ == '__main__':
-	time_delay = 1
-	query_type = 'district_code' # set it to "pincode" to query by pincode
+
+	# config file path
 	config_file_path = 'config.yml'
-	
-	print("querying by {} .....".format(query_type))
-	## root url and headers
-	root_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public"
-	headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
-
-	vaccineSpotter = vaccineSpotter(config_file_path, time_delay)
-	vaccineSpotter.query(root_url, headers, query_type)
-
-	while True:
-		delta = datetime.now()-t
-		if delta.seconds >= time_delay * 60:
-			vaccineSpotter.query(root_url, headers, query_type)
-			t = datetime.now()
+	vaccineSpotter = vaccineSpotter(config_file_path)
+	vaccineSpotter.main()
